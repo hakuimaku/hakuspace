f8e4c15c6aa6f433ed258c7161102914a05e58d4
hakuimaku <tienbao246@gmail.com>
2026-06-17 17:05:23 +0700

    update waybar - when apply-style.sh: use reload_style_on_change instead of killall waybar, use symlink for waybar config

--- local/bin/apply-style.sh
+++ local/bin/apply-style.sh
@@ -27,18 +27,7 @@ fi
 hyprctl reload >/dev/null 2>&1 || true
 
 # Waybar reload
-DIR_TOP="$HOME/.config/waybar/waybartop"
-STATE_FILE="/tmp/waybar_current_mode"
-CURRENT_STATE=$(cat "$STATE_FILE")
-
-killall waybar
-while pgrep -x waybar >/dev/null; do sleep 0.1; done
-
-if [ "$CURRENT_STATE" == "left" ]; then
-    waybar & 
-else
-    waybar -c "$DIR_TOP/config" -s "$DIR_TOP/style.css" &
-fi
+# Oh we don't need it anymore, it uses reload_style_on_change
 
 # Swaync reload
 pkill swaync >/dev/null 2>&1 || true
