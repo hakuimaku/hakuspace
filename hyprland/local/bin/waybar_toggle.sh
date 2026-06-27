5c605985b3b977c77488c21e78cfd0da5a092a83
hakuimaku <tienbao246@gmail.com>
2026-06-27 09:15:25 +0700

    update waybar_toggle.sh: add argument --reset for reset waybar tmp file if it works weird

--- hyprland/local/bin/waybar_toggle.sh
+++ hyprland/local/bin/waybar_toggle.sh
@@ -4,8 +4,12 @@ DIR_LEFT="$HOME/.config/waybar/waybarleft"
 DIR_TOP="$HOME/.config/waybar/waybartop"
 STATE_FILE="/tmp/waybar_current_mode"
 
+if [ $1 == "--reset" ]; then
+    sudo rm "$STATE_FILE"
+fi
+
 if [ ! -f "$STATE_FILE" ]; then
-    echo "left" > "$STATE_FILE"
+    echo "top" > "$STATE_FILE"
 fi
 
 CURRENT_STATE=$(cat "$STATE_FILE")
@@ -13,16 +17,16 @@ CURRENT_STATE=$(cat "$STATE_FILE")
 killall waybar
 while pgrep -x waybar >/dev/null; do sleep 0.1; done
 
-if [ "$CURRENT_STATE" == "left" ]; then
-    # Link configuration Waybar Top to root (waybar/) and run
-    ln -sf "$DIR_TOP/config" "$HOME/.config/waybar/config"
-    ln -sf "$DIR_TOP/style.css" "$HOME/.config/waybar/style.css"
-    waybar &
-    echo "top" > "$STATE_FILE"
-else
-    # Link configuration Waybar Left to root and run
+if [ "$CURRENT_STATE" == "top" ]; then
+    # Link configuration Waybar Left to root (waybar/) and run
     ln -sf "$DIR_LEFT/config" "$HOME/.config/waybar/config"
     ln -sf "$DIR_LEFT/style.css" "$HOME/.config/waybar/style.css"
-    waybar & 
+    waybar &
     echo "left" > "$STATE_FILE"
+else
+    # Link configuration Waybar Top to root and run
+    ln -sf "$DIR_TOP/config" "$HOME/.config/waybar/config"
+    ln -sf "$DIR_TOP/style.css" "$HOME/.config/waybar/style.css"
+    waybar & 
+    echo "top" > "$STATE_FILE"
 fi
