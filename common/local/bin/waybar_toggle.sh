45c23b6accaf991cb7559c234bbbba1b3a47e74e
hakuimaku <tienbao246@gmail.com>
2026-06-29 15:09:39 +0700

    update waynbar_toggle.sh: add argument --start for auto start waybar, will not toggle top & left when startup

--- common/local/bin/waybar_toggle.sh
+++ common/local/bin/waybar_toggle.sh
@@ -3,18 +3,33 @@
 DIR_LEFT="$HOME/.config/waybar/waybarleft"
 DIR_TOP="$HOME/.config/waybar/waybartop"
 STATE_FILE="/tmp/waybar_current_mode"
+CURRENT_STATE=$(cat "$STATE_FILE")
 
+# Reset Waybar mode to top by removing the state file
 if [ $1 == "--reset" ]; then
     echo "Remove state file ($STATE_FILE) to reset Waybar mode to top"
     sudo rm "$STATE_FILE"
 fi
 
+# Create the state file if it doesn't exist and set the default mode to top
 if [ ! -f "$STATE_FILE" ]; then
     echo "top" > "$STATE_FILE"
 fi
 
-CURRENT_STATE=$(cat "$STATE_FILE")
+# Start Waybar with the current mode
+if [ $1 == "--start" ]; then
+    if [ "$CURRENT_STATE" == "top" ]; then
+        ln -sf "$DIR_TOP/config" "$HOME/.config/waybar/config"
+        ln -sf "$DIR_TOP/style.css" "$HOME/.config/waybar/style.css"
+    else
+        ln -sf "$DIR_LEFT/config" "$HOME/.config/waybar/config"
+        ln -sf "$DIR_LEFT/style.css" "$HOME/.config/waybar/style.css"
+    fi
+    waybar &
+    exit 0
+fi
 
+# Toggle Waybar mode between top and left
 killall waybar
 while pgrep -x waybar >/dev/null; do sleep 0.1; done
 
