2cb3716072918c52f326fbb7790e6107a53e68f9
hakuimaku <tienbao246@gmail.com>
2026-07-04 16:36:24 +0700

    fix install.sh: remove install waybar-cava & remove allow xhost display

--- install.sh
+++ install.sh
@@ -464,8 +464,6 @@ sudo systemctl disable getty@tty1.service
 
 xdg-mime default thunar.desktop inode/directory
 
-xhost +si:localuser:root
-
 $HOME/.local/bin/gen-style.sh
 
 echo "✅ All services have been processed!"
