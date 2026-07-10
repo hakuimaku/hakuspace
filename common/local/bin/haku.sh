5633cd052662fdea655c590cf9952ac2219dcf3f
hakuimaku <tienbao246@gmail.com>
2026-07-10 19:59:18 +0700

    update script haku.sh: some change for hyprland scrolling layout & improve code

--- common/local/bin/haku.sh
+++ common/local/bin/haku.sh
@@ -6,40 +6,64 @@ FONT_TERMINAL=15
 
 spawn() { ( setsid "$@" & ) >/dev/null 2>&1; }
 
-
-if [[ $1 == "--clear" ]]; then
+clear() {
     PIDS=$(pgrep -f "seycava|seylavat|seyclock|seycmd")
 
     for pid in $PIDS; do
         echo "Killing window with PID: $pid"
         kill -9 "$pid"
     done
+}
+
+cava() {
+    spawn kitty --title "hakucava" --class "seycava" -o font_size=$FONT_GENERAL sh -c "cava"
+    sleep 0.2
+}
+
+lavat() {
+    spawn kitty --title "hakulavat" --class "seylavat" -o font_size=$FONT_GENERAL sh -c "lavat -c blue -k blue -r1"
+    sleep 0.2
+}
+
+clock() {
+    spawn kitty --title "hakuclock" --class "seyclock" -o font_size=$FONT_CLOCK sh -c "tty-clock -c -C 4 -r -b"
+    sleep 0.2
+}
+
+cmd() {
+    spawn kitty --title "hakucmd" --class "seycmd" -o font_size=$FONT_TERMINAL --hold fastfetch
+    sleep 0.2
+}
+
+
+if [[ $1 == "--clear" ]]; then
+    clear
     exit 0
 fi
 
+# Main
 if [[ $XDG_CURRENT_DESKTOP == "Hyprland" ]]; then
-    hyprctl eval 'hl.dispatch(hl.dsp.focus({ workspace = "1" }))'
-
     MY_INFO=$(hyprctl activewindow -j)
     MY_ADDR=$(echo "$MY_INFO" | jq -r '.pid')
-    TARGETS=$(hyprctl clients -j | jq -r --arg addr "$MY_ADDR" '.[] | select(.workspace.id == 1) | .pid')
-
-    TARGETS=$(echo "$TARGETS" | grep -v "$MY_ADDR")
-    for win in $TARGETS; do
-        echo "Killing window with address: $win"
-        kill -9 "$win"
-    done
+    
+    clear
     sleep 0.1
 
-    spawn kitty --title "hakucava" --class "seycava" -o font_size=$FONT_GENERAL sh -c "cava"
-    sleep 0.1
-    spawn kitty --title "hakulavat" --class "seylavat" -o font_size=$FONT_GENERAL sh -c "lavat -c blue -k blue -r1"
-    sleep 0.1
-    spawn kitty --title "hakuclock" --class "seyclock" -o font_size=$FONT_CLOCK sh -c "tty-clock -c -C 4 -r -b"
-    sleep 0.1
-    spawn kitty --title "hakucmd" --class "seycmd" -o font_size=$FONT_TERMINAL --hold fastfetch
+    cmd
 
-    sleep 0.5
+    clock
+    hyprctl eval 'hl.dispatch(hl.dsp.focus({ window = "class:seyclock" }))'
+
+    lavat
+    hyprctl eval 'hl.dispatch(hl.dsp.focus({ window = "class:seyclock" }))'
+    hyprctl eval 'hl.dispatch(hl.dsp.layout("consume"))'
+
+    cava
+    hyprctl eval 'hl.dispatch(hl.dsp.focus({ window = "class:seyclock" }))'
+    hyprctl eval 'hl.dispatch(hl.dsp.layout("consume"))'
+
+    hyprctl eval 'hl.dispatch(hl.dsp.focus({ window = "class:seycmd" }))'
+
     kill -9 "$MY_ADDR"
 fi
 
@@ -47,22 +71,18 @@ if [[ $XDG_CURRENT_DESKTOP == "niri" ]]; then
     MY_INFO=$(niri msg focused-window)
     MY_ADDR=$(echo "$MY_INFO" | grep "PID:" | awk '{print $2}')
 
-    niri msg action focus-workspace 1
+    clear
+    sleep 0.1
 
-    spawn kitty --title "hakucmd" --class "seycmd" -o font_size=$FONT_TERMINAL --hold fastfetch
-    sleep 0.2
+    cmd
 
-    spawn kitty --title "hakuclock" --class "seyclock" -o font_size=$FONT_CLOCK sh -c "tty-clock -c -C 4 -r -b"
+    clock
 
-    sleep 0.2
-
-    spawn kitty --title "hakulavat" --class "seylavat" -o font_size=$FONT_GENERAL sh -c "lavat -c blue -k blue -r1"
-    sleep 0.2
+    lavat
     niri msg action focus-column-left
     niri msg action consume-window-into-column
     
-    spawn kitty --title "hakucava" --class "seycava" -o font_size=$FONT_GENERAL sh -c "cava"
-    sleep 0.2
+    cava
     niri msg action focus-column-left
     niri msg action consume-window-into-column
 
