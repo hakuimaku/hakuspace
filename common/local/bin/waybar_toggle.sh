0ac19c41e5934f0b538f11d4c2f20755b48ebf19
hakuimaku <tienbao246@gmail.com>
2026-07-13 17:49:22 +0700

    update waybar_toggle.sh: streamline linking functions and improve state management

--- common/local/bin/waybar_toggle.sh
+++ common/local/bin/waybar_toggle.sh
@@ -5,46 +5,57 @@ DIR_TOP="$HOME/.config/waybar/waybartop"
 STATE_FILE="/tmp/waybar_current_mode"
 CURRENT_STATE=$(cat "$STATE_FILE")
 
-# Reset Waybar mode to top by removing the state file
-if [ $1 == "--reset" ]; then
-    echo "Remove state file ($STATE_FILE) to reset Waybar mode to top"
-    sudo rm "$STATE_FILE"
-fi
+link_top() {
+    ln -sf "$DIR_TOP/config" "$HOME/.config/waybar/config"
+    ln -sf "$DIR_TOP/style.css" "$HOME/.config/waybar/style.css"
+}
+
+link_left() {
+    ln -sf "$DIR_LEFT/config" "$HOME/.config/waybar/config"
+    ln -sf "$DIR_LEFT/style.css" "$HOME/.config/waybar/style.css"
+}
+
+run_waybar() {
+    if [ "$CURRENT_STATE" == "top" ]; then
+        link_top
+    elif [ "$CURRENT_STATE" == "left" ]; then
+        link_left
+    else
+        notify-send "Waybar Toggle" "Error: Invalid state in $STATE_FILE"
+        exit 1
+    fi
+    waybar &
+}
 
 # Create the state file if it doesn't exist and set the default mode to top
 if [ ! -f "$STATE_FILE" ]; then
     echo "top" > "$STATE_FILE"
+    CURRENT_STATE="top"
 fi
 
-# Start Waybar with the current mode
-if [ $1 == "--start" ]; then
-    if [ "$CURRENT_STATE" == "top" ]; then
-        ln -sf "$DIR_TOP/config" "$HOME/.config/waybar/config"
-        ln -sf "$DIR_TOP/style.css" "$HOME/.config/waybar/style.css"
-    else
-        ln -sf "$DIR_LEFT/config" "$HOME/.config/waybar/config"
-        ln -sf "$DIR_LEFT/style.css" "$HOME/.config/waybar/style.css"
-    fi
-    waybar &
+# Check if there is no config or style file in the root
+if [ ! -f "$HOME/.config/waybar/config" ] || [ ! -f "$HOME/.config/waybar/style.css" ]; then
+    link_top
+fi
+
+# Check if Waybar is not running (for auto start)
+if ! pgrep -x waybar >/dev/null; then
+    run_waybar
     exit 0
 fi
 
-# Toggle Waybar mode between top and left
+
+# Main logic to toggle Waybar mode between top and left
 killall waybar
-while pgrep -x waybar >/dev/null; do sleep 0.1; done
 
 if [ "$CURRENT_STATE" == "top" ]; then
-    # Link configuration Waybar Left to root (waybar/) and run
-    ln -sf "$DIR_LEFT/config" "$HOME/.config/waybar/config"
-    ln -sf "$DIR_LEFT/style.css" "$HOME/.config/waybar/style.css"
-    waybar &
     echo "left" > "$STATE_FILE"
+    CURRENT_STATE="left"
+    run_waybar
 else
-    # Link configuration Waybar Top to root and run
-    ln -sf "$DIR_TOP/config" "$HOME/.config/waybar/config"
-    ln -sf "$DIR_TOP/style.css" "$HOME/.config/waybar/style.css"
-    waybar & 
     echo "top" > "$STATE_FILE"
+    CURRENT_STATE="top"
+    run_waybar
 fi
 
 # In Niri, kill cava underbar when changing waybar mode
