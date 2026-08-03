48e9348e55c04ef70d22e1491e89f74024a5c5f4
hakuimaku <tienbao246@gmail.com>
2026-08-03 07:38:22 +0700

    Refactor exit.sh: improve exit confirmation and cleanup process for window manager

--- common/local/bin/exit.sh
+++ common/local/bin/exit.sh
@@ -2,13 +2,17 @@
 
 # This script handles exiting the current window manager
 
-# Confirm exit
 if ! rofi -dmenu -p "Are you sure you want to exit?" -theme-str 'window { width: 30%; height: 30%; } entry { placeholder: ""; }' <<< "Yes
 No" | grep -q "Yes"; then
     exit 0
 fi
 
-# 1. Clean up running applications
+# STOP SYSTEMD GRAPHICAL TARGETS FIRST
+systemctl --user stop graphical-session.target 2>/dev/null
+systemctl --user stop graphical-session-pre.target 2>/dev/null
+systemctl --user stop xdg-desktop-portal.service 2>/dev/null
+
+# APP LIST TO KILL
 APP_LIST=(
     "code" "code-url-handler" "zen" "zen-bin" "firefox" "chromium" "kitty" "slurp"
     "waybar" "dockbar" "hypridle" "swaync" "sway-audio-idle-inhibit"
@@ -19,13 +23,24 @@ APP_PATTERN=$(IFS="|" ; echo "${APP_LIST[*]}")
 pkill -SIGTERM -u "$USER" -f "$APP_PATTERN" 2>/dev/null
 killall -q xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-wlr xdg-desktop-portal-gtk xdg-desktop-portal-gnome 2>/dev/null
 
-sleep 0.3
+# WAIT FOR DATA SAVING
+sleep 1.5
+
+# FORCE KILL STUBBORN PROCESSES
 pkill -9 -u "$USER" -f "$APP_PATTERN" 2>/dev/null
 
-pkill -SIGTERM -u "$USER" -x "Xwayland" 2>/dev/null
-rm -f /tmp/.X11-unix/X* 2>/dev/null
+# Note: Avoid manually killing Xwayland. The active compositor should manage and kill its own Xwayland instance.
+# pkill -SIGTERM -u "$USER" -x "Xwayland" 2>/dev/null
 
-# Exit current window manager
+# CLEANUP LOCKS AND SPECIFIC SOCKETS
+rm -f /tmp/.X11-unix/X* 2>/dev/null
+rm -f /tmp/.X*-lock 2>/dev/null
+rm -rf /tmp/hypr /tmp/niri* /tmp/sway* /tmp/waybar* /tmp/steam* 2>/dev/null
+
+# UNSET ENVIRONMENT VARIABLES
+systemctl --user unset-environment WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP 2>/dev/null
+
+# EXIT CURRENT WINDOW MANAGER
 if [[ $XDG_CURRENT_DESKTOP == "Hyprland" ]]; then
     hyprctl eval 'hl.dispatch(hl.dsp.exit())'
 elif [[ $XDG_CURRENT_DESKTOP == "niri" ]]; then
@@ -35,15 +50,3 @@ elif [[ $XDG_CURRENT_DESKTOP == "mango" ]]; then
 elif [[ $XDG_CURRENT_DESKTOP == "labwc"  ]]; then
     labwc --exit
 fi
-
-# Stop the graphical session
-systemctl --user stop graphical-session.target 2>/dev/null
-systemctl --user stop graphical-session-pre.target 2>/dev/null
-
-# Unset environment variables
-systemctl --user unset-environment WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP 2>/dev/null
-
-# Clean /tmp directory
-rm -rf /tmp/.X11-unix /tmp/.X*-lock 2>/dev/null
-rm -rf /tmp/hypr /tmp/niri* /tmp/sway* /tmp/waybar* /tmp/steam* 2>/dev/null
-find /tmp -maxdepth 1 -user "$USER" ! -name "." -exec rm -rf {} + 2>/dev/null
