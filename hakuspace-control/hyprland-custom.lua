21a1d5303d453590e7aaade45716594666d1a884
hakuimaku <tienbao246@gmail.com>
2026-08-07 08:20:19 +0700

    Update hyprland-custom.lua: add note for change keybindings

--- hakuspace-control/hyprland-custom.lua
+++ hakuspace-control/hyprland-custom.lua
@@ -15,9 +15,6 @@
 
 -- See more in the Wiki: https://wiki.hypr.land/
 
--- Example Keybinding:
-hl.bind("SUPER + Return", hl.dsp.exec_cmd("kitty"))
-
 -- Example Rule:
 local thunar_rule = hl.window_rule({
     name  = "thunar-floating",
@@ -27,20 +24,18 @@ local thunar_rule = hl.window_rule({
     center = true,
     size  = "600 600",
 })
-thunar_rule:set_enabled(false)
+thunar_rule:set_enabled(false) -- You can enable/disable rules
 
 -- On/off rules. See more in ~/.config/hypr/config/rule.lua
 fullWidth_ScrollingLayout:set_enabled(true) -- Set width 100% for certain apps
 specificWidth_ScrollingLayout:set_enabled(true) -- Set width 60% for certain apps
 
--- Use my plugin config: (require enable hyprexpo)
-hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
-
-hl.on("hyprland.start", function () 
-    hl.exec_cmd("hyprpm reload -n")
-end)
-
-hl.bind("SUPER + G", function() hl.plugin.hyprexpo.expo("toggle") end)
-
--- Make sure you install & enable hyprexpo plugin to use this, uncomment the line below.
+-- Use my plugin config: (require hyprexpo | Make sure you have hyprexpo installed & enabled)
+--hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
+--hl.on("hyprland.start", function () hl.exec_cmd("hyprpm reload -n") end)
+--hl.bind("SUPER + G", function() hl.plugin.hyprexpo.expo("toggle") end)
 --require("config/plugin")
+
+-- Example change keybind for SUPER + E
+-- hl.unbind("SUPER + E") -- Unbind SUPER + E: Thunar open (Based on my default config)
+-- hl.bind("SUPER + E", hl.dsp.exec_cmd("discord")) -- Bind SUPER + E: Discord open (Bind new command)
