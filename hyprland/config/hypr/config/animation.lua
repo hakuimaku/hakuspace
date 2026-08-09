1c56bab658e84673213b674555665d674b744185
hakuimaku <tienbao246@gmail.com>
2026-08-09 07:26:20 +0700

    Update Hyprland animation: curves and styles in animation.lua for improved visual effects

--- hyprland/config/hypr/config/animation.lua
+++ hyprland/config/hypr/config/animation.lua
@@ -10,14 +10,14 @@
 --------------------------
 ------- ANIMATIONS -------
 --------------------------
-hl.curve( "myBezier", {type = "bezier", points = { {0.05, 0.9}, {0.1, 1.05} } })
+hl.curve( "myBezier", {type = "bezier", points = { {0.02, 1.0}, {0.3, 1.05} } })
 hl.curve( "easeInOut", { type = "bezier", points = { {0.65, 0}, {0.35, 1} } })
-hl.curve( "smoothzz", { type = "bezier", points = { {0.1, 0.8}, {0.24, 1} } })
+hl.curve( "smoothzz", { type = "bezier", points = { {0.2, 0.9}, {0.2, 1} } })
 
 -- Window
-hl.animation({ leaf = "windows", enabled = true, duration = 1, speed = 6, bezier = "myBezier", style = "popin 80%" })
-hl.animation({ leaf = "windowsIn", enabled = true, duration = 1, speed = 6, bezier = "myBezier", style = "popin 80%" })
-hl.animation({ leaf = "windowsOut", enabled = true, duration = 1, speed = 12, bezier = "myBezier", style = "popin 80%" })
+hl.animation({ leaf = "windows", enabled = true, duration = 1, speed = 6, bezier = "myBezier", style = "gnomed" })
+hl.animation({ leaf = "windowsIn", enabled = true, duration = 1, speed = 6, bezier = "myBezier", style = "gnomed" })
+hl.animation({ leaf = "windowsOut", enabled = true, duration = 1, speed = 12, bezier = "myBezier", style = "gnomed" })
 hl.animation({ leaf = "windowsMove", enabled = true, duration = 1, speed = 8, bezier = "myBezier" })
 
 -- Layer
