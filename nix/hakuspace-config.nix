dab97091ad02a8ecb63033f7661c2f1bb793c2df
hakuimaku <tienbao246@gmail.com>
2026-08-13 08:36:53 +0700

    Update NixOS: fix repository URL and enhance configuration options

--- nix/hakuspace-config.nix
+++ nix/hakuspace-config.nix
@@ -6,144 +6,158 @@
 #                              /_/                      
 #   NixOS configuration file for hakuspace
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+    cfg = config.hakuspace;
+in
 {
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-    
-    # Programs ================================================
-    nixpkgs.config.allowUnfree = true;
-    programs.firefox.enable = true;
-
-    # Zsh Shell
-    programs.zsh.enable = true;
-    users.defaultUserShell = pkgs.zsh;
-
-    # Thunar File Manager
-    programs.xfconf.enable = true;
-    programs.thunar.enable = true;
-    programs.thunar.plugins = with pkgs; [
-        thunar-archive-plugin
-        thunar-volman
-    ];
-    services.udisks2.enable = true;
-    services.gvfs.enable = true;
-    services.tumbler.enable = true;
-    security.polkit.enable = true;
-
-    # Nano Editor
-    programs.nano = {
-        enable = true;
-        syntaxHighlight = true;
-        nanorc = ''
-            set tabsize 4
-            set tabstospaces
-            set mouse
-        '';
+    options.hakuspace = {
+        enable = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Enable Hakuspace NixOS base configuration";
+        };
+        
+        enableDefaultShell = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Set Zsh as default user shell";
+        };
     };
 
-    # Hyprland and Niri
-    programs.hyprland.enable = true;
-    #programs.niri.enable = true;
+    config = lib.mkIf cfg.enable {
+        # Experimental features
+        nix.settings.experimental-features = lib.mkDefault [ "nix-command" "flakes" ];
+        
+        # Hardware Graphics for Wayland Compositors
+        hardware.graphics.enable = lib.mkDefault true;
 
-    # Packages ================================================
-    environment.systemPackages = with pkgs; [
-        (python3.withPackages (ps: with ps; [
-            colorthief
-            pygobject3
-        ]))
-        gtk3
-        pango
-        gdk-pixbuf
-        atk
-        harfbuzz
-        gobject-introspection
-        gtk-layer-shell
-        glib
-        libnotify
-        file
-        git
-        vscode
-        waybar
-        rofi
-        kitty
-        swaynotificationcenter
-        fastfetch
-        awww
-        mpvpaper
-        hyprlock
-        hypridle
-        hyprpicker
-        hyprsunset
-        gammastep
-        jq
-        imagemagick
-        wlr-randr
-        wl-clipboard
-        cliphist
-        grim
-        slurp
-        mpv
-        imv
-        nwg-look
-        xdg-utils
-        mate-polkit
-        xhost
-        xwayland-satellite
-        file-roller
-        p7zip
-        unrar
-        unzip
-        zip
-        rofi-emoji
-        wl-screenrec
-        sway-audio-idle-inhibit
-        wget
-        cava
-        tty-clock
-        lavat
-        chafa
-        pipes
-        cmatrix
-        brightnessctl
-        ncdu
-        gparted
-        btop
-        pavucontrol
-        mangowc
-        labwc
-    ];
+        # Programs ================================================
+        nixpkgs.config.allowUnfree = lib.mkDefault true;
+        programs.firefox.enable = lib.mkDefault true;
 
-    fonts = {
-        fontconfig.enable = true;
-        packages = with pkgs; [
-            noto-fonts
-            noto-fonts-cjk-sans
-            noto-fonts-color-emoji
-            nerd-fonts.jetbrains-mono
+        # Zsh Shell
+        programs.zsh.enable = lib.mkDefault true;
+        users.defaultUserShell = lib.mkIf cfg.enableDefaultShell (lib.mkDefault pkgs.zsh);
+
+        # Thunar File Manager
+        programs.xfconf.enable = lib.mkDefault true;
+        programs.thunar.enable = lib.mkDefault true;
+        programs.thunar.plugins = with pkgs; [
+            thunar-archive-plugin
+            thunar-volman
         ];
-    };
+        services.udisks2.enable = lib.mkDefault true;
+        services.gvfs.enable = lib.mkDefault true;
+        services.tumbler.enable = lib.mkDefault true;
+        security.polkit.enable = lib.mkDefault true;
 
-    xdg.portal = {
-        enable = true;
-        extraPortals = [
-        pkgs.xdg-desktop-portal-gtk
-        pkgs.xdg-desktop-portal-wlr 
-        pkgs.xdg-desktop-portal-gnome
+        # Nano Editor
+        programs.nano = {
+            enable = lib.mkDefault true;
+            syntaxHighlight = true;
+            nanorc = ''
+                set tabsize 4
+                set tabstospaces
+                set mouse
+            '';
+        };
+
+        # Desktop Environment / Window Manager
+        programs.hyprland.enable = lib.mkDefault true;
+
+        # Packages ================================================
+        environment.systemPackages = with pkgs; [
+            (python3.withPackages (ps: with ps; [
+                colorthief
+                pygobject3
+            ]))
+            gtk3
+            pango
+            gdk-pixbuf
+            atk
+            harfbuzz
+            gobject-introspection
+            gtk-layer-shell
+            glib
+            libnotify
+            file
+            git
+            vscode
+            waybar
+            rofi
+            kitty
+            swaynotificationcenter
+            fastfetch
+            awww
+            mpvpaper
+            hyprlock
+            hypridle
+            hyprpicker
+            hyprsunset
+            gammastep
+            jq
+            imagemagick
+            wlr-randr
+            wl-clipboard
+            cliphist
+            grim
+            slurp
+            mpv
+            imv
+            nwg-look
+            xdg-utils
+            mate-polkit
+            xhost
+            xwayland-satellite
+            file-roller
+            p7zip
+            unrar
+            unzip
+            zip
+            rofi-emoji
+            wl-screenrec
+            sway-audio-idle-inhibit
+            wget
+            cava
+            tty-clock
+            lavat
+            chafa
+            pipes
+            cmatrix
+            brightnessctl
+            ncdu
+            gparted
+            btop
+            pavucontrol
+            mangowc
+            labwc
         ];
 
-        config = {
-            hyprland = {
-                default = [ "hyprland" "gtk" ];
-            };
-            niri = {
-                default = [ "gnome" "gtk" ];
-            };
-            mango = {
-                default = [ "wlr" "gtk" ];
-            };
-            labwc = {
-                default = [ "wlr" "gtk" ];
+        fonts = {
+            fontconfig.enable = lib.mkDefault true;
+            packages = with pkgs; [
+                noto-fonts
+                noto-fonts-cjk-sans
+                noto-fonts-color-emoji
+                nerd-fonts.jetbrains-mono
+            ];
+        };
+
+        xdg.portal = {
+            enable = lib.mkDefault true;
+            extraPortals = [
+                pkgs.xdg-desktop-portal-gtk
+                pkgs.xdg-desktop-portal-wlr 
+                pkgs.xdg-desktop-portal-gnome
+            ];
+
+            config = lib.mkDefault {
+                hyprland.default = [ "hyprland" "gtk" ];
+                niri.default = [ "gnome" "gtk" ];
+                mango.default = [ "wlr" "gtk" ];
+                labwc.default = [ "wlr" "gtk" ];
             };
         };
     };
