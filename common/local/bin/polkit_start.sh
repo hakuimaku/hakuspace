5e7f97d0a1d6191c71cdafe24b74e935475badff
hakuimaku <tienbao246@gmail.com>
2026-08-14 09:15:07 +0700

    Update polkit_start.sh: enhance script to manage polkit-mate authentication agent

--- common/local/bin/polkit_start.sh
+++ common/local/bin/polkit_start.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
-/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 || \
-/run/current-system/sw/libexec/polkit-mate-authentication-agent-1 || \
-/usr/libexec/polkit-mate-authentication-agent-1
+
+killall -q polkit-mate-authentication-agent-1
+while pgrep -u $UID -x polkit-mate-authentication-agent-1 >/dev/null; do sleep 0.1; done
+
+if [ -f /run/current-system/sw/libexec/polkit-mate-authentication-agent-1 ]; then
+    exec /run/current-system/sw/libexec/polkit-mate-authentication-agent-1
+elif POLKIT_BIN=$(ls -d /nix/store/*-mate-polkit-*/libexec/polkit-mate-authentication-agent-1 2>/dev/null | head -n 1) && [ -n "$POLKIT_BIN" ]; then
+    exec "$POLKIT_BIN"
+elif [ -f /usr/libexec/polkit-mate-authentication-agent-1 ]; then
+    exec /usr/libexec/polkit-mate-authentication-agent-1
+else
+    echo "Error: mate-polkit binary not found!" >&2
+    notify-send "Error: mate-polkit binary not found!"
+    exit 1
+fi
