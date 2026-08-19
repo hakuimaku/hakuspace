0fbc985ee1a8d0bbf6cbac37af4eedd769fa893a
hakuimaku <tienbao246@gmail.com>
2026-08-19 15:55:29 +0700

    fix (polkit_start.sh): add Fedora mate polkit execute

--- src/common/local/bin/polkit_start.sh
+++ src/common/local/bin/polkit_start.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# NixOS
 if POLKIT_BIN=$(ls -d /nix/store/*-mate-polkit-*/libexec/polkit-mate-authentication-agent-1 2>/dev/null | head -n 1) && [ -n "$POLKIT_BIN" ]; then
     exec "$POLKIT_BIN"
+# Arch Linux
 elif [ -f /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 ]; then
     exec /usr/lib/mate-polkit/polkit-mate-authentication-agent-1
+# Fedora
+elif [ -f /usr/libexec/polkit-mate-authentication-agent-1 ]; then
+    exec /usr/libexec/polkit-mate-authentication-agent-1
 else
     echo "Error: mate-polkit binary not found!" >&2
     notify-send "Error: mate-polkit binary not found!"
