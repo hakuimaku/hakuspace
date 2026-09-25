ee940ae8167547ae62dabda8e590f92975d95e8a
hakuimaku <tienbao246@gmail.com>
2026-09-25 14:31:47 +0700

    startup: reorder 4 wm mini-apps startup

--- src/home/.config/hypr/config/autostart.lua
+++ src/home/.config/hypr/config/autostart.lua
@@ -25,11 +25,13 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("blueman-applet")
     hl.exec_cmd("hypridle")
     hl.exec_cmd("fcitx5 -d")
-    hl.exec_cmd("$HOME/.local/bin/waybar_manager.sh") -- Run waybar
-    hl.exec_cmd("$HOME/.local/bin/taskbar_manager.sh --startup") -- Run taskbar if state is true
-    hl.exec_cmd("$HOME/.local/bin/desktop_icons_manager.sh --startup") -- Run desktop icons if state is true
+
     hl.exec_cmd("$HOME/.local/bin/rounded_screen_manager.sh --startup") -- Run rounded screen if state is true
     hl.exec_cmd("$HOME/.local/bin/edge_trigger_manager.sh --startup") -- Run edge trigger if state is true
+    hl.exec_cmd("$HOME/.local/bin/taskbar_manager.sh --startup") -- Run taskbar if state is true
+    hl.exec_cmd("$HOME/.local/bin/waybar_manager.sh") -- Run waybar
+    hl.exec_cmd("$HOME/.local/bin/desktop_icons_manager.sh --startup") -- Run desktop icons if state is true
+
     hl.exec_cmd("$HOME/.local/bin/welcome.sh")
 end)
 
